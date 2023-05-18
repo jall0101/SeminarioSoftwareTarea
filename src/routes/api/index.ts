a15585598478855b39c1bc847ea8cdd64fc9a24e
jall0101 <92348560+jall0101@users.noreply.github.com>
2023-05-17 22:10:20 -0600

    Miércoles 17 de mayo

---
+++ src/routes/api/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const router = express.Router();
+import projectsRouter from './projects'
+import securityRouter from './security'
+import teamsRouter from './teams'
+
+router.use('/projects', projectsRouter)
+router.use('/security', securityRouter)
+router.use('/security', teamsRouter)
+
+export default router;
