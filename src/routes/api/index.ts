09976a537f22ac67e89088535a29a9a2f58e9526
jall0101 <92348560+jall0101@users.noreply.github.com>
2023-05-22 18:09:10 -0600

    Jueves 18 de mayo

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -8,4 +8,18 @@ router.use('/projects', projectsRouter)
 router.use('/security', securityRouter)
 router.use('/security', teamsRouter)
 
+// api/projects/echo/hola?variable1=a&variable2=b
+router.get('/echo/:msg',(req, res) => {
+    const{ msg } = req.params;
+    const { variable1='Hola', variable2='Mundo'} = req.query;
+    res.json({msg, variable1, variable2});
+});
+
+
+
+router.post('/echo2/:msg',(req, res) => {
+    const{ msg } = req.params;
+    const { variable1='Hola', variable2='Mundo'} = req.body;
+    res.json({msg, variable1, variable2});
+});
 export default router;
